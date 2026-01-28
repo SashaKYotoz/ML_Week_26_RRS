6425afbf34903d44adc97b01c621378a02f56018
chro_m <75805637+Andrew-Chrom@users.noreply.github.com>
2026-01-28 22:54:18 +0200

    task: add metric to temp/ReccomendsFilter.py

--- temp/RecommendsFilter.py
+++ temp/RecommendsFilter.py
@@ -106,3 +106,78 @@ class RecommendsFilter:
         recommendations = candidates.sort_values("match_score", ascending=True)
 
         return recommendations["name"].head(n_recommendations).tolist()
+    
+def compute_ndcg_at_k(predicted_list, truth_dict, k):
+    dcg = 0.0
+    idcg = 0.0
+    
+    for i, item_id in enumerate(predicted_list[:k]):
+        rel = truth_dict.get(item_id, 0)
+        if rel > 0:
+            dcg += (2 ** rel - 1) / np.log2(i + 2)
+            
+
+    ideal_ratings = sorted(truth_dict.values(), reverse=True)
+    
+    for i, rel in enumerate(ideal_ratings[:k]):
+        idcg += (2 ** rel - 1) / np.log2(i + 2)
+        
+    if idcg == 0:
+        return 0.0
+        
+    return dcg / idcg
+
+def evaluate_CB_model(val_path, train_path, recipes_path, k=5):
+    val_df = pd.read_csv(val_path).fillna(2.5)
+    recipes_df = pd.read_csv(recipes_path).fillna(2.5)
+    
+    
+    name_to_id = pd.Series(recipes_df.recipe_id.values, index=recipes_df.name).to_dict()
+    
+    recommender = RecommendsFilter()
+    
+    ndcg_scores = []
+
+    unique_users = val_df['user_id'].unique()
+    
+    n_users_limit = len(unique_users)
+    if n_users_limit:
+        unique_users = unique_users[:n_users_limit]
+        
+    print(f"Num of users {len(unique_users)}...")
+    
+    for i, user_id in enumerate(unique_users):
+        user_val_data = val_df[val_df['user_id'] == user_id]
+
+        truth_dict = dict(zip(user_val_data['recipe_id'], user_val_data['rating']))
+
+        try:
+            rec_names = recommender.get_recipe_recommendations(user_id, n_recommendations=k, users_path="student_data/users.csv", recipes_path="student_data/recipes.csv")
+            
+            # user based recs
+            # rec_names = recommender.get_interaction_recommendations(
+            #     user_id=user_id,
+            #     n_recommendations=k,
+            #     interactions_path=train_path, 
+            #     recipes_path=recipes_path
+            # )
+        except Exception as e:
+            print(f"Error for user {user_id}: {e}")
+            rec_names = []
+            
+        rec_ids = [name_to_id.get(name) for name in rec_names if name in name_to_id]
+        
+        score = compute_ndcg_at_k(rec_ids, truth_dict, k)
+        ndcg_scores.append(score)
+        
+    mean_ndcg = np.mean(ndcg_scores)
+    print(f"Avg NDCG@{k}: {mean_ndcg:.4f}")
+    return mean_ndcg
+
+if __name__ == "__main__":
+    evaluate_CB_model(
+        val_path="student_data/interactions_val.csv",       
+        train_path="student_data/interactions_train.csv",   
+        recipes_path="student_data/recipes.csv",                      
+        k=5                                    
+    )
